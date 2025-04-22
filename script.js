80500ae59c0654aa5341e740c0f3033594b0e7d5
matheus olavo <olavo6013@gmail.com>
2025-04-22 20:10:45 -0300

    Implementa a função adicionarGasto para criar e adicionar um novo item à lista no DOM

--- script.js
+++ script.js
@@ -6,9 +6,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     formGasto.addEventListener('submit', function(event) {
         event.preventDefault();
-        
+
         const descricao = document.getElementById('descricao').value;
         const valor = parseFloat(document.getElementById('valor').value);
         const categoria = document.getElementById('categoria').value;
+        
+    if (descricao && !isNaN(valor)) {
+            adicionarGasto(descricao, valor, categoria);
+            formGasto.reset();
+        } else {
+            alert('Por favor, preencha a descrição e um valor válido.');
+        }
     });
+
+    function adicionarGasto(descricao, valor, categoria) {
+        const novoItem = document.createElement('li');
+        novoItem.innerHTML = `
+            <div class="gasto-info">
+                <span>${descricao}</span> -
+                <span>R$ ${valor.toFixed(2)}</span>
+                <small>(${categoria})</small>
+            </div>
+            <button class="remover-gasto-btn">x</button>
+        `;
+        listaGastos.appendChild(novoItem);
+    }
 });
