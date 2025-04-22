fb6a8ac992a536daaa4cf3c5e4d95d82741a1785
matheus olavo <olavo6013@gmail.com>
2025-04-22 20:08:30 -0300

    Obtém os valores de descrição, valor e categoria do formulário no submit

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const listaGastos = document.getElementById('lista-gastos');
     const totalElement = document.getElementById('total');
     let totalGasto = 0;
+
     formGasto.addEventListener('submit', function(event) {
         event.preventDefault();
+        
+        const descricao = document.getElementById('descricao').value;
+        const valor = parseFloat(document.getElementById('valor').value);
+        const categoria = document.getElementById('categoria').value;
     });
 });
