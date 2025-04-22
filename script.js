41e0d597bc3b51d9e78dd910c6125c091c765da5
matheus olavo <olavo6013@gmail.com>
2025-04-22 20:02:58 -0300

    Seleciona elementos do DOM necessários (formulário, lista, total)

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formGasto = document.getElementById('form-gasto');
+    const listaGastos = document.getElementById('lista-gastos');
+    const totalElement = document.getElementById('total');
+    let totalGasto = 0;
+});
